37542b2e7581a17fca2b968590787ceb5730cea9
Nick Badger <nbadger1@gmail.com>
2025-06-06 19:34:01 +0200

    Remove dead code

--- sidecars_py/templatey_testutils/src_py/templatey_testutils/__init__.py
+++ sidecars_py/templatey_testutils/src_py/templatey_testutils/__init__.py
@@ -1,8 +1,6 @@
-from collections.abc import Iterable
 from unittest.mock import Mock
 
 from templatey.interpolators import NamedInterpolator
-from templatey.parser import InterpolatedVariable
 from templatey.templates import TemplateConfig
 
 
@@ -19,29 +17,3 @@ zderr_template_config = TemplateConfig(
     variable_escaper=Mock(
         spec=_variable_escaper_spec, side_effect=ZeroDivisionError()),
     content_verifier=Mock(wraps=lambda value: True))
-
-
-class FakeComplexContent:
-    TEMPLATEY_CONTENT = True
-
-    def __init__(self, keyword: str, noun: str):
-        self._keyword = keyword
-        self._noun = noun
-
-    def flatten(
-            self,
-            unescaped_vars_context: dict[str, int],
-            parent_part_index: int,
-            ) -> Iterable[str | InterpolatedVariable]:
-        """This is a very simple kind of complex content that... well,
-        constructs a correct plural of the word dog based on the
-        ``dog_count`` variable.
-        """
-        is_plural = unescaped_vars_context[self._keyword] > 1
-        yield InterpolatedVariable(
-            name=self._keyword, format_spec=None, conversion=None,
-            part_index=parent_part_index)
-        if is_plural:
-            yield f' {self._noun}s'
-        else:
-            yield f' {self._noun}'
